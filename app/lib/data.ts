d52af56a33056b5e02f93d6f3374443ff86b794f
lilraja-x <rajauzair184@gmail.com>
2024-02-25 00:18:24 +0500

    commit

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,7 +35,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
@@ -63,7 +63,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-
+ 
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
